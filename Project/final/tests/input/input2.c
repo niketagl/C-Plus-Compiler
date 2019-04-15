2a79550cdd52b45cf47ff5c713ccfe9105462ae5
nidanshu7 <nidanshu7@gmail.com>
2019-04-15 17:46:02 +0530

    function label added

--- Project/final/tests/input/input2.c
+++ Project/final/tests/input/input2.c
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-int fib(int n)
+int fun(int a)
 {
-	if(n==0 || n==1)
-		return 1;
-	return fib(n-1) + fib(n-2);
-=======
-int fibbonacci(int n) {
-   	if(n == 0){
-   		return 0;
-   	} else if(n == 1) {
-   		return 1;
-   	} else {
-   		return (fibbonacci(n-1) + fibbonacci(n-2));
-   	}
->>>>>>> 1f0aa5746291433b17284fbde42b4e3e6cc51c6d
+  a = a + a;
+  return a;
 }
 
-int main() {
-    int n = 5;
-    int i;
-	int a = fibbonacci(n);
+int main()
+{
+  int x,y;
+  x = x*y;
+  return 0;
 }
